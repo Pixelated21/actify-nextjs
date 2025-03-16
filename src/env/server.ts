c7ac5072639bc8b515c6feb4551b444b11e7d841
Proximity <javaughnbailey21@gmail.com>
2025-03-16 01:30:05 -0500

    Refactor server environment configuration by removing the PORT variable and streamlining the structure for SERVER_API_BASE_URL and FASTIFY_API_KEY.

--- src/env/server.ts
+++ src/env/server.ts
@@ -2,15 +2,13 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const serverEnv = createEnv({
-  server: {
-    PORT: z.string().min(1).default("3000"),
-    SERVER_API_BASE_URL: z.string().min(1).url(),
-    FASTIFY_API_KEY: z.string().min(1),
-  },
+	server: {
+		SERVER_API_BASE_URL: z.string().min(1).url(),
+		FASTIFY_API_KEY: z.string().min(1),
+	},
 
-  runtimeEnv: {
-    PORT: process.env.PORT,
-    SERVER_API_BASE_URL: process.env.SERVER_API_BASE_URL,
-    FASTIFY_API_KEY: process.env.FASTIFY_API_KEY,
-  },
+	runtimeEnv: {
+		SERVER_API_BASE_URL: process.env.SERVER_API_BASE_URL,
+		FASTIFY_API_KEY: process.env.FASTIFY_API_KEY,
+	},
 });
